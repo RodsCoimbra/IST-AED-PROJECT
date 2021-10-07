2a45112a2872367223ebed4d6d969856db35bf45
Rodrigo Coimbra <rodscoimbra@gmail.com>
2021-10-07 22:22:57 +0100

    O 6 é fodido, crl

--- Enunciado_A1/main.c
+++ Enunciado_A1/main.c
@@ -9,10 +9,10 @@ int FA2 (int** maze, int l, int c, int lin, int col);
 int FA3 (int** maze, int l, int c, int lin, int col);
 int FA4 (int** maze, int l, int c, int lin, int col);
 int FA5 (int** maze, int l, int c, int lin, int col);
-/*int FA6 (int** maze, int l, int c, int lin, int col);*/
+/*int FA6 (int** maze, int l, int c, int lin, int col, int l2, int c2);*/
 int out (int l, int c, int lin, int col);
 
-int mod(int** maze, int l, int c, int lin, int col,char* modo){
+int mod(int** maze, int l, int c, int lin, int col,char* modo, int l2, int c2){
     int resp;
     if((strcmp(modo,"A1"))==0){
         resp = FA1(maze, l, c, lin, col);
@@ -34,9 +34,9 @@ int mod(int** maze, int l, int c, int lin, int col,char* modo){
         resp = FA5(maze, l, c, lin, col);
         return resp;
     }
-    /*else if((strcmp(modo,"A6"))==0){
-        resp = FA6(maze, l, c, lin, col);
-        return resp;}*/
+    else if((strcmp(modo,"A6"))==0){
+        //resp = FA6(maze, l, c, lin, col, l2, c2);
+        return -5;}
     else
     {
     exit(-1);
@@ -59,7 +59,7 @@ void separar(char* str)
 
 int main(int argc, char *argv[]) {
 opterr = 0;
-int opt, lin = 0, col = 0, parede, l=0, c=0, tamanho = 0, linaux=0, colaux=0;
+int opt, lin = 0, col = 0, parede, l=0, c=0, tamanho = 0,linaux=0, colaux=0;
 tamanho = strlen(argv[2])+1;
 char* fileread =(char*) calloc(1,tamanho*sizeof(char)), *filewrite = (char*) calloc(1,(tamanho+1)*sizeof(char));
 if (fileread == NULL || filewrite == NULL)
@@ -93,7 +93,16 @@ if ((fsol = fopen(filewrite, "w")) == NULL)      // Se der erro ao abrir o fiche
          exit(-1);
         }
 
-fscanf(fmaze, "%d %d %d %d %s %d", &lin, &col, &l, &c, modo, &parede);
+fscanf(fmaze, "%d %d %d %d %s", &lin, &col, &l, &c, modo);
+int l2 = 0, c2 = 0;
+if((strcmp(modo,"A6"))!=0)
+    {
+    fscanf(fmaze,"%d", &parede);
+    }
+else
+    {
+    fscanf(fmaze,"%d %d %d",&l2, &c2, &parede);
+    }
 linaux = lin;
 colaux = col;
 int** maze =(int**) malloc (lin*sizeof(int*));
@@ -114,11 +123,19 @@ fscanf(fmaze, "%d %d %d", &l1, &c1, &custo);
 maze[l1-1][c1-1] = custo;
 }
 int resposta; 
-resposta = mod(maze, l-1, c-1, lin-1, col-1, modo);
+resposta = mod(maze, l-1, c-1, lin-1, col-1, modo, l2, c2);
 fprintf(fsol,"%d\n\n", resposta);
 
-while (1){  
-fscanf(fmaze, "%d %d %d %d %s %d", &lin, &col, &l, &c, modo, &parede);
+while (1){
+fscanf(fmaze, "%d %d %d %d %s", &lin, &col, &l, &c, modo);
+if((strcmp(modo,"A6"))!=0)
+    {
+    fscanf(fmaze,"%d", &parede);
+    }
+else
+    {
+    fscanf(fmaze,"%d %d %d",&l2, &c2, &parede);
+    } 
 if(feof(fmaze) != 0) break;
 if(lin != linaux){
 maze =(int**) realloc (maze,lin*sizeof(int*));
@@ -147,7 +164,7 @@ for(int i=0, l1, c1, custo; i < parede; i++)
 fscanf(fmaze, "%d %d %d", &l1, &c1, &custo);
 maze[l1-1][c1-1] = custo;
 }
-resposta = mod(maze, l-1, c-1, lin-1, col-1, modo);
+resposta = mod(maze, l-1, c-1, lin-1, col-1, modo, l2, c2);
 fprintf(fsol,"%d\n\n", resposta);
 }
 free(fileread);
@@ -208,5 +225,9 @@ int FA5 (int** maze, int l, int c, int lin, int col) {
 }
 
  /*int FA6 (int** maze, int l, int c, int lin, int col) {
-
+        if (maze[l][c]==0 && maze[l2[c2]==0] && (out(l,c,lin,col)!=-2) && (out(l2,c2,lin,col)!=-2)){
+            for (maze[l][c]!=maze[l+1][c] && maze[l][c]!=maze[l][c+1]){
+            }
+        }
+        else {return 0;}
  }*/
