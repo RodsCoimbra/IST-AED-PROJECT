ede90c5484d9e41f5ed8e87378f54188badde3e8
Rodrigo Coimbra <rodscoimbra@gmail.com>
2021-10-15 16:49:38 +0100

    Funcemina e tirei também as funções antigas!!!!!!

--- As.c
+++ As.c
@@ -62,14 +62,6 @@ int FA5 (int** maze, int l, int c, int lin, int col) { /*indica se a célula de 
     sz[i] = 1;
     id[i] = i;
     }
-    /*for (int p = 0; p <= lin; p++){
-        for(int q = 0; q <= col; q++){
-        i = p*(col+1) + q;
-        printf("%2d ", id[i]);
-        }
-        printf("\n");
-    }*/
-
     for (int p = 0; p <= lin; p++) {
         for(int q = 0; q <= col-1; q++){
             i = p*(col+1) + q;
@@ -98,23 +90,6 @@ int FA5 (int** maze, int l, int c, int lin, int col) { /*indica se a célula de 
     }
     }
     }
-    /*for (int q = 0; q <= col; q++) {
-        for(int p = 0; p <= lin-1; p++){
-            if(maze[p][q] == 0 && maze[p+1][q] == 0){
-            for (i = p*(col) + q; i!=id[i] ; i = id[i]);
-            for (j = p*(col) + q + 1; j!=id[j]; j = id[j]);
-        }
-    if (i == j) continue;
-    if (sz[i] < sz[j]) {
-     id[i] = j; 
-     sz[j] += sz[i];
-    } 
-    else {
-        id[j] = i; 
-        sz[i] += sz[j];
-    }
-    }
-    }*/
     for(i =(l*(col+1))+c; i!= id[i]; i = id[i]);
     for(j =(l2*(col+1))+c2; j!=id[j]; j = id[j]);
 
